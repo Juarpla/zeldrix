dde02eeef3dae24888585464d9e699ceb2e1f83b
Juan Plasencia <jplasencia.dev@outlook.com>
2026-05-25 13:39:12 -0500

    Initial commit of local project files

---
+++ src/app/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+
+export default function Home() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main className="container">
+      <h1>Welcome to Tauri + Next.js</h1>
+
+      <div className="row">
+        <a href="https://nextjs.org" target="_blank">
+          <img src="/next.svg" className="logo next" alt="Next.js logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src="/react.svg" className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <p>Click on the Tauri, Next.js, and React logos to learn more.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+      <p>{greetMsg}</p>
+    </main>
+  );
+}
